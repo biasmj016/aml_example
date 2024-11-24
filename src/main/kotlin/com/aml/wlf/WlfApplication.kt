88a52e38a25711a04fde1b281cd087a01b1beb8a
biasmj016 <mjkim031@gmail.com>
2024-11-24 16:38:40 +0900

    기본 프로젝트 세팅

---
+++ src/main/kotlin/com/aml/wlf/WlfApplication.kt
@@ -0,0 +1,11 @@
+package com.aml.wlf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WlfApplication
+
+fun main(args: Array<String>) {
+    runApplication<WlfApplication>(*args)
+}
---
+++ src/test/kotlin/com/aml/wlf/WlfApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aml.wlf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WlfApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
