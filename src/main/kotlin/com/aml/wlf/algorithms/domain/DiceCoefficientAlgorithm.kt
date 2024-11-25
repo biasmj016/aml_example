ea08151451c6e6c3fa04fe937b49b34b3ec551ff
biasmj016 <mjkim031@gmail.com>
2024-11-25 14:45:49 +0900

    feat(algorithm) : 다이스 스코어 유사도 알고리즘 추가

---
+++ src/main/kotlin/com/aml/wlf/algorithms/domain/DiceCoefficientAlgorithm.kt
@@ -0,0 +1,30 @@
+package com.aml.wlf.algorithms.domain
+
+import kotlin.math.round
+
+object DiceCoefficientAlgorithm {
+    fun calculateSimilarity(base: String, comparison: String): Double {
+        val baseMap = toMap(base)
+        val comparisonMap = toMap(comparison)
+        val totalSize = baseMap.size + comparisonMap.size
+
+        if (totalSize == 0) return 0.0
+
+        val intersection = baseMap.keys.intersect(comparisonMap.keys).size
+
+        return toPercentage(2.0 * intersection / totalSize)
+    }
+
+    private fun toMap(text: String): Map<String, Int> {
+        val regex = "[^a-zA-Z0-9]".toRegex()
+        return text.split("\\s+".toRegex())
+            .mapNotNull { it.replace(regex, "")
+                .takeIf(String::isNotEmpty)?.lowercase() }
+            .groupingBy { it }
+            .eachCount()
+    }
+
+    private fun toPercentage(value: Double): Double {
+        return round(value * 10000) / 100
+    }
+}
---
+++ src/test/kotlin/com/aml/wlf/algorithms/domain/DiceCoefficientAlgorithmTest.kt
@@ -0,0 +1,38 @@
+package com.aml.wlf.algorithms.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DiceCoefficientAlgorithmTest {
+    @Test
+    fun calculate_success() {
+        val base = "test name"
+        val comparison = "test name"
+        val result = DiceCoefficientAlgorithm.calculateSimilarity(base, comparison)
+        assertEquals(100.0, result)
+    }
+
+    @Test
+    fun calculate_fail() {
+        val base = "test name"
+        val comparison = "other thing"
+        val result = DiceCoefficientAlgorithm.calculateSimilarity(base, comparison)
+        assertEquals(0.0, result)
+    }
+
+    @Test
+    fun calculate_similar() {
+        val base = "test name"
+        val comparison = "other name"
+        val result = DiceCoefficientAlgorithm.calculateSimilarity(base, comparison)
+        assertEquals(50.0, result)
+    }
+
+    @Test
+    fun calculate_empty() {
+        val base = ""
+        val comparison = ""
+        val result = DiceCoefficientAlgorithm.calculateSimilarity(base, comparison)
+        assertEquals(0.0, result)
+    }
+}
