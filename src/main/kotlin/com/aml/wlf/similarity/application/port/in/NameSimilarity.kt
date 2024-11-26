87880adf0fbd5647e2c9f33a7e813252bb4bb234
biasmj016 <mjkim031@gmail.com>
2024-11-26 21:03:05 +0900

    feat(algorithm) : 이름 유사도 계산 usecase 추가(임시)

---
+++ src/main/kotlin/com/aml/wlf/similarity/application/port/in/NameSimilarity.kt
@@ -0,0 +1,32 @@
+package com.aml.wlf.similarity.application.port.`in`
+
+import com.aml.wlf.algorithms.domain.*
+import org.springframework.stereotype.Service
+
+interface NameSimilarity {
+    fun calculate(base: String, comparison: String, algorithms: List<Algorithm>): Double
+
+    @Service
+    class NameSimilarityUsecase : NameSimilarity {
+
+        private val similarityStrategies = mapOf(
+            "CosineSimilarity" to CosineSimilarityAlgorithm::calculateSimilarity,
+            "DiceCoefficient" to DiceCoefficientAlgorithm::calculateSimilarity,
+            "HammingDistance" to HammingDistanceAlgorithm::calculateSimilarity,
+            "JaccardSimilarity" to JaccardSimilarityAlgorithm::calculateSimilarity,
+            "JaroWinklerDistance" to JaroWinklerDistanceAlgorithm::calculateSimilarity
+        )
+
+        override fun calculate(base: String, comparison: String, algorithms: List<Algorithm>): Double {
+            val totalWeight = algorithms.sumOf { it.weight }
+            if (totalWeight == 0.0) return 0.0
+
+            return algorithms.sumOf { it.weightedSimilarity(base, comparison) } / totalWeight
+        }
+
+        private fun Algorithm.weightedSimilarity(base: String, comparison: String): Double {
+            val similarity = similarityStrategies[type]?.invoke(base, comparison) ?: 0.0
+            return similarity * weight
+        }
+    }
+}
---
+++ src/test/kotlin/com/aml/wlf/similarity/application/port/in/NameSimilarityTest.kt
@@ -0,0 +1,67 @@
+package com.aml.wlf.similarity.application.port.`in`
+
+import com.aml.wlf.algorithms.domain.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NameSimilarityTest {
+    @Autowired
+    private lateinit var usecase: NameSimilarity.NameSimilarityUsecase
+
+    @Test
+    fun calculate_low() {
+        val base = "test name"
+        val comparison = "abcd efgh"
+        val algorithms = listOf(
+            Algorithm(1L, "CosineSimilarity", 0.3),
+            Algorithm(2L, "DiceCoefficient", 0.2),
+            Algorithm(3L, "HammingDistance", 0.1),
+            Algorithm(4L, "JaccardSimilarity", 0.2),
+            Algorithm(5L, "JaroWinklerDistance", 0.2)
+        )
+
+        val result = usecase.calculate(base, comparison, algorithms)
+        System.out.println(result)
+
+        val expectedSimilarity = (
+                CosineSimilarityAlgorithm.calculateSimilarity(base, comparison) * 0.3 +
+                        DiceCoefficientAlgorithm.calculateSimilarity(base, comparison) * 0.2 +
+                        HammingDistanceAlgorithm.calculateSimilarity(base, comparison) * 0.1 +
+                        JaccardSimilarityAlgorithm.calculateSimilarity(base, comparison) * 0.2 +
+                        JaroWinklerDistanceAlgorithm.calculateSimilarity(base, comparison) * 0.2
+                ) / 1.0
+
+        System.out.println(expectedSimilarity)
+        assertEquals(expectedSimilarity, result)
+    }
+
+    @Test
+    fun calculate_high() {
+        val base = "example"
+        val comparison = "example"
+        val algorithms = listOf(
+            Algorithm(1L, "CosineSimilarity", 0.3),
+            Algorithm(2L, "DiceCoefficient", 0.2),
+            Algorithm(3L, "HammingDistance", 0.1),
+            Algorithm(4L, "JaccardSimilarity", 0.2),
+            Algorithm(5L, "JaroWinklerDistance", 0.2)
+        )
+
+        val result = usecase.calculate(base, comparison, algorithms)
+        System.out.println(result)
+
+        val expectedSimilarity = (
+                CosineSimilarityAlgorithm.calculateSimilarity(base, comparison) * 0.3 +
+                        DiceCoefficientAlgorithm.calculateSimilarity(base, comparison) * 0.2 +
+                        HammingDistanceAlgorithm.calculateSimilarity(base, comparison) * 0.1 +
+                        JaccardSimilarityAlgorithm.calculateSimilarity(base, comparison) * 0.2 +
+                        JaroWinklerDistanceAlgorithm.calculateSimilarity(base, comparison) * 0.2
+                ) / 1.0
+
+        System.out.println(expectedSimilarity)
+        assertEquals(expectedSimilarity, result)
+    }
+}
