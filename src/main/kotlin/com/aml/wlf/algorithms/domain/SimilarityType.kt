e71cddd035bdff5b3f648a57e08fd26137b72661
biasmj016 <mjkim031@gmail.com>
2024-11-28 15:49:24 +0900

    feat(algorithm) : 가중치 상세화

--- src/main/kotlin/com/aml/wlf/algorithms/domain/SimilarityType.kt
+++ src/main/kotlin/com/aml/wlf/algorithms/domain/SimilarityType.kt
@@ -1,9 +1,9 @@
 package com.aml.wlf.algorithms.domain
 
 enum class SimilarityType(val weight: Double) {
-    COSINE_SIMILARITY(30.0),
+    COSINE_SIMILARITY(15.0),
     DICE_COEFFICIENT(20.0),
-    HAMMING_DISTANCE(10.0),
-    JACCARD_SIMILARITY(20.0),
-    JARO_WINKLER_DISTANCE(20.0)
+    HAMMING_DISTANCE(5.0),
+    JACCARD_SIMILARITY(25.0),
+    JARO_WINKLER_DISTANCE(35.0)
 }
