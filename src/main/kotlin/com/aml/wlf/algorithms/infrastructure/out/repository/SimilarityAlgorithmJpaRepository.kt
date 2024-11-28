80883c4c7eb50cbb9e2d2bc79dc4138fdc62dbc7
biasmj016 <mjkim031@gmail.com>
2024-11-28 15:51:24 +0900

    feat(algorithm) : 안쓰는 엔티티, 레포지토리 삭제

--- src/main/kotlin/com/aml/wlf/algorithms/infrastructure/out/repository/SimilarityAlgorithmJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.aml.wlf.algorithms.infrastructure.out.repository
-
-import com.aml.wlf.algorithms.infrastructure.out.repository.entity.SimilarityAlgorithm
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SimilarityAlgorithmJpaRepository : JpaRepository<SimilarityAlgorithm, Long> {
-}
